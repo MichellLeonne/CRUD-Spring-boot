0e20d2c14c725c5344feca682b66ac126e75927b
Michell <michel_leone@exactus.com.br>
2024-10-18 17:11:38 -0300

    create table bd

--- src/main/java/cursoSpring/api/conreoller/MedicoController.java
+++ src/main/java/cursoSpring/api/conreoller/MedicoController.java
@@ -3,7 +3,9 @@ package cursoSpring.api.conreoller;
 import cursoSpring.api.endereco.Endereco;
 import cursoSpring.api.medico.DadosCadastroMedico;
 import cursoSpring.api.medico.MedicoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.reactive.TransactionalOperator;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,11 @@ public class MedicoController {
 
     @Autowired
     private MedicoRepository repository;
+    @Autowired
+    private TransactionalOperator transactionalOperator;
 
     @PostMapping
+    @Transactional
     public void Cadastrar(@RequestBody DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
 
