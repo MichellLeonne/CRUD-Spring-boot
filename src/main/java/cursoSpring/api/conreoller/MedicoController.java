88de7f046206fb95facda9c3f3bf04c0eed2d935
MichellLeonne <michell.leonne@gmail.com>
2024-10-22 22:59:31 -0300

    requisicao Get

--- src/main/java/cursoSpring/api/conreoller/MedicoController.java
+++ src/main/java/cursoSpring/api/conreoller/MedicoController.java
@@ -1,16 +1,17 @@
 package cursoSpring.api.conreoller;
 
-import cursoSpring.api.endereco.Endereco;
 import cursoSpring.api.medico.DadosCadastroMedico;
+import cursoSpring.api.medico.DadosListagemMedico;
 import cursoSpring.api.medico.MedicoRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.reactive.TransactionalOperator;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping ("/medicos")
@@ -26,4 +27,10 @@ public class MedicoController {
         repository.save(new Medico(dados));
 
     }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10 , sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
+
 }
